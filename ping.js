e7f3b143d2c6c94d3f3779372bd2a18c8cd2d604
Ankcorn <thomasankcorn@gmail.com>
2023-02-01 17:48:27 +0000

    seeding

--- ping.js
+++ ping.js
@@ -2,57 +2,59 @@ const axios = require("axios");
 const util = require("util");
 
 const wait = util.promisify(setTimeout);
-const url = 'https://a43hiiwt6d.execute-api.eu-west-1.amazonaws.com/prod/'
+const url = "https://a43hiiwt6d.execute-api.eu-west-1.amazonaws.com/prod/";
 
 function random(min, max) {
-  return Math.ceil(Math.random() * (max- min) + min)
+	return Math.ceil(Math.random() * (max - min) + min);
 }
 function search(term) {
-  return axios.get(`${url}search?term${term}`)
+	return axios.get(`${url}search?term${term}`);
 }
 
 function get(name) {
-  return axios.get(`${url}pokemons/${name}`)
+	return axios.get(`${url}pokemons/${name}`);
 }
 
 function scan() {
-  return axios.get(`${url}pokemons/`)
+	return axios.get(`${url}pokemons/`);
 }
 
 function chunkArray(array, chunkSize) {
-  return Array.from(
-    { length: Math.ceil(array.length / chunkSize) },
-    (_, index) => array.slice(index * chunkSize, (index + 1) * chunkSize)   
-  );
+	return Array.from(
+		{ length: Math.ceil(array.length / chunkSize) },
+		(_, index) => array.slice(index * chunkSize, (index + 1) * chunkSize),
+	);
 }
 
 async function ping() {
-  const pokemons = ["Bulbasaur", "Spearow", "Nidorino", "Zubat", "Dugtrio"];
+	const pokemons = ["Bulbasaur", "Spearow", "Nidorino", "Zubat", "Dugtrio"];
 
-  const promises = ['search', 'scan', 'get'].flatMap((el => Array(random(10, 100)).fill(el))).sort(() => Math.random() - 0.5)
-  console.log()
+	const promises = ["search", "scan", "get"]
+		.flatMap((el) => Array(random(10, 100)).fill(el))
+		.sort(() => Math.random() - 0.5);
 
-  for(let reqs of chunkArray(promises, 3)) {
-    try {
-      const res = await Promise.all(reqs.map((el) => {
-        if(el === 'search') {
-          return search(pokemons[random(0, pokemons.length)])
-        }
-        if(el === 'get') {
-          return get(pokemons[random(0, pokemons.length)])
-        }
-        if(el === 'scan') {
-          return scan()
-        }
-      }))
-      console.log(res.map(el => el.data))
-    } catch(e) {
-      console.log(e)
-    }
-    
+	for (let reqs of chunkArray(promises, 3)) {
+		try {
+			const res = await Promise.all(
+				reqs.map((el) => {
+					if (el === "search") {
+						return search(pokemons[random(0, pokemons.length - 1)]);
+					}
+					if (el === "get") {
+						return get(pokemons[random(0, pokemons.length - 1)]);
+					}
+					if (el === "scan") {
+						return scan();
+					}
+				}),
+			);
+			console.log(res.map((el) => el.data));
+		} catch (e) {
+			console.log(e);
+		}
 
-    await wait(50)
-  }
+		await wait(50);
+	}
 }
 
-module.exports = ping
+module.exports = ping;
